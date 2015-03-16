907b05652079a05579a4120b599e9175c46e4fe1
thaishamilton <thaismha@gmail.com>
2015-03-16 01:56:11 -0300

    destroy only for destroy articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
 
   http_basic_authenticate_with name: "dhh", password: "secret",
-                               except: [:index, :show]
+                               except: [:index, :show],
+                               only: :destroy
 
   def new
     @article = Article.new
